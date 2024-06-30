f0f03a558cb7d68a276794db98302a7b64f2fc32
Sadie Parker <sadieparker@byui.edu>
2024-06-29 21:24:40 -0600

    first changes

---
+++ prove/Develop03/Scripture.cs
@@ -0,0 +1,14 @@
+
+using System;
+
+public class Scripture
+    {
+        public string Reference;
+        public string Text;
+
+        public Scripture(string reference, string text)
+        {
+            Reference = reference;
+            Text = text;
+        }
+    }
---
+++ prove/Develop03/Word.cs
@@ -0,0 +1,13 @@
+using System;
+
+public class Word
+    {
+        public string Text;
+        public bool Hidden;
+
+        public Word(string text)
+        {
+            Text = text;
+            Hidden = false;
+        }
+    }
